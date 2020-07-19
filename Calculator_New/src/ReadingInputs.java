74f6b8a9034bfb8d345d5aeb84bf3b781f43b6ba
Vikash Yadav <vikash.yadav9911@gmail.com>
2020-07-19 07:46:54 +0530

    Updated

--- Calculator_New/src/ReadingInputs.java
+++ Calculator_New/src/ReadingInputs.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class ReadingInputs {
 
 	public String readInputString(String message) {
-		Scanner intText = new Scanner(System.in);
-		String yMsg = intText.nextLine();
+		Scanner inpText = new Scanner(System.in);
+		String yMsg = inpText.nextLine();
 
 		return "Dummy Value";
 	}
 
 	public int readInputNumber(String message) {
-		Scanner intNum = new Scanner(System.in);
-		int num1 = intNum.nextInt();
+		Scanner inpNum = new Scanner(System.in);
+		int num1 = inpNum.nextInt();
 
 		return 0;
 	}
